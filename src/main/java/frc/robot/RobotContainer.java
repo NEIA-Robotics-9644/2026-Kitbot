53a43fc95ad836780f5b4d5c327a05b7c7061ad6
hydrobyte-x <hydrobyte.dev@gmail.com>
2026-01-21 16:18:52 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,28 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
 
-    public RobotContainer(){
-        //add things here
-    }
+  private final Alert driverDisconnected =
+      new Alert("Driver controller disconnected (port 0).", AlertType.kWarning);
+  private final Alert operatorDisconnected =
+      new Alert("Operator controller disconnected (port 1).", AlertType.kWarning);
+  private final Alert noAutoAlert =
+      new Alert("Please select an auto routine!!! 😳", AlertType.kError);
 
-    public Command getAutonomousCommand() {
+  private Command noAuto = Commands.none();
+
+  // Controller
+  private final CommandXboxController driverCon = new CommandXboxController(0);
+  private final CommandXboxController operatorCon = new CommandXboxController(1);
+
+  public RobotContainer(){
+      //add things here
+   }
+
+  public Command getAutonomousCommand() {
     return autoChooser.get();
   }
 
