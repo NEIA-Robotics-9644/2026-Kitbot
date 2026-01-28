8cdc5a0fb20c03fb0050e97124494b8e28ff93a2
Jeremie-desgin <jkwapong27@neiacademy.org>
2026-01-28 15:57:29 -0500

    Update DriveSubsystem.java

--- src/main/java/frc/robot/subsystems/DriveTrain/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrain/DriveSubsystem.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems.DriveTrain;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-//import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -13,18 +15,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DriveSubsystem extends SubsystemBase {
 
   // Motors I Included followers now 
-  private final SparkMax m_leftLeader = new SparkMax(1, null);
-  private final SparkMax m_leftFollower = new SparkMax(2, null);
+  private final SparkMax m_leftLeader = new SparkMax(1,MotorType.kBrushless);
+  private final SparkMax m_leftFollower = new SparkMax(2, MotorType.kBrushless);
   
-  private final SparkMax m_rightLeader = new SparkMax(3, null);
-  private final SparkMax m_rightFollower = new SparkMax(4, null);
+  private final SparkMax m_rightLeader = new SparkMax(3, MotorType.kBrushless);
+  private final SparkMax m_rightFollower = new SparkMax(4, MotorType.kBrushless);
 
   private final DifferentialDrive m_drive = new DifferentialDrive(m_leftLeader, m_rightLeader);
 
+  private final SparkMaxConfig config = new SparkMaxConfig();
 
   public DriveSubsystem(){
 
-    m_leftLeader.setInverted(true);
+    config.inverted(true);
+    m_leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
   }
 
