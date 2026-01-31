1562c9aa39ff9239307492864cd766dc7429ac7c
Jeremie-desgin <jkwapong27@neiacademy.org>
2026-01-30 20:01:32 -0500

    Uodate

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -2,11 +2,37 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-//import frc.robot.subsystems.CANFuelSubsystem;
-//import static frc.robot.Constants.FueConstants.*;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.DriveTrain.CANDriveSubsystem;
 
 public class DriveCommand extends Command{
-    
+
+    CANDriveSubsystem driveSubsystem;
+    CommandXboxController controller;
+
+    public DriveCommand(CANDriveSubsystem driveSystem, CommandXboxController drivecontroller){
+        addRequirements(driveSubsystem);
+        driveSubsystem = driveSystem;
+        controller = drivecontroller;
+    }
+
+    @Override
+    public void initialize(){
+    }
+
+    @Override
+    public void execute(){
+        driveSubsystem.driveArcade(-controller.getLeftY(), -controller.getRightX());
+    }
+    @Override 
+    public void end(boolean interrupted){
+        driveSubsystem.driveArcade(0, 0);
+    }
+    @Override
+    public boolean isFinished(){
+        return false;
+
+    }
     }
     
 
